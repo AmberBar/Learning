c8e486f3413843fdaec8c00848eb05b989f5306e
Amber Lei <amberlei@augmentum.com.cn>
2018-10-17 16:15:04 +0800

    nio channel

--- NioDemo/src/com/amber/test/NonBlockNioTest.java
+++ NioDemo/src/com/amber/test/NonBlockNioTest.java
@@ -45,20 +45,24 @@ public class NonBlockNioTest {
 		try {
 			serverSocketChannel = ServerSocketChannel.open();
 			serverSocketChannel.configureBlocking(false);
+			//创建Selector对象
 			Selector selector = Selector.open();
+			//把serverSocketChannel交给Selector管理，并绑定监听状态OP_ACCEPT
 			serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 			serverSocketChannel.bind(new InetSocketAddress(9898));
 			while (selector.select() > 0) {
+				//获得迭代器
 				Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-
 				while (iterator.hasNext()) {
-					
 					SelectionKey selectionKey = iterator.next();
-				
+					//判断channel是否是 is ready to accept准备
 					if (selectionKey.isAcceptable()) {
-
+						//服务器连接请求！！！这个时候才连接，而不是像阻塞Io那样，不管三七二十一直接连接请求
 						SocketChannel socketChannel = serverSocketChannel.accept();
+						//设置成非阻塞
 						socketChannel.configureBlocking(false);
+						//注册channel到selector,这里注意，SocketChannel是一个新的渠道也需要注册
+						//监听read
 						socketChannel.register(selector, SelectionKey.OP_READ);
 					} else if (selectionKey.isReadable()){
 						//获取当前选择器中读就绪的channel
@@ -67,10 +71,12 @@ public class NonBlockNioTest {
 						int len = 0;
 						while ((len = socketChannel.read(buffer) )!= -1) {
 							buffer.flip();
+							//这里可以把客户端传过来的byte做一些转化
 							System.out.println(buffer);
 							buffer.clear();
 						}
 					}
+					//从迭代器中把已经完成是事件移除
 					iterator.remove();
 				}
 			}
@@ -78,6 +84,5 @@ public class NonBlockNioTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 }
