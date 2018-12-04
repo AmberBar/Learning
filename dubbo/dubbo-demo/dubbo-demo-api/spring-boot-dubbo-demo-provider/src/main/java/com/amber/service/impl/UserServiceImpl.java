db575643458144d8312164a8f1864e0252802144
amberlei <949144210@qq.com>
2018-12-05 07:43:48 +0800

    spring boot dubbo

---
+++ dubbo/dubbo-demo/dubbo-demo-api/spring-boot-dubbo-demo-provider/src/main/java/com/amber/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.amber.service.impl;
+
+import com.amber.bean.Address;
+
+import com.amber.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@com.alibaba.dubbo.config.annotation.Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<Address> findAllAddressByUserId(Integer userId) {
+        List<Address> addresses = new ArrayList<Address>();
+        addresses.add(new Address(1,"上海", 1));
+        addresses.add(new Address(2,"北京", 1));
+        return addresses;
+    }
+}
